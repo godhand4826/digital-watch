83013b501de5e1e3d25b1cb9cd44b9e31c3c0f7a
Eric Huang <godhand4826@gmail.com>
2018-11-20 20:25:27 +0800

    add DigitalClock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import DigitalClock from "./DigitalClock";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <DigitalClock />
       </div>
     );
   }
---
+++ src/DigitalClock.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class DigitalClock extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div>DIGITAL CLOCK</div>);
+    }
+}
+
+export default DigitalClock;
